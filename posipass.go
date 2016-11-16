9bc66be59f053e08910c997aaa24ca615fa91885
Dave Webb <github@d5ve.com>
2016-11-16 22:39:18 +1300

    Start of commanline arg parsing

--- posipass.go
+++ posipass.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+    "flag"
+)
+
+var help = flag.Bool("help", false, "Print help docs")
+var words = flag.Int("words", 3, "Number of words in each passphrase")
+var gen = flag.Int("gen", 10, "Number of passphrases to generate")
+
+flag.Parse()
+
 func main() {
 
 }
